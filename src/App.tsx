58663b47de9300398a267c460b74c4e12e99c61c
Vitor <vitorbarbosa1998@hotmail.com>
2020-06-30 17:19:46 +0100

    feat: added grid card

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import React from "react";
 import Card from "./components/Card";
 
+import "./App.css";
+
 function App() {
   return (
     <div>
       <h1>Example</h1>
-      <Card />
+      <div className="cards">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
     </div>
   );
 }
