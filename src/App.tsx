e31264539c49f5cfca75c2a01d46716d6175e5c0
Vitor <vitorbarbosa1998@hotmail.com>
2020-07-03 11:36:56 +0100

    fead: added header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Card from "./components/Card";
+import Header from "./components/Header";
 
 import "./App.css";
 
 function App() {
   return (
     <div>
+      <Header />
       <h1>Example</h1>
       <div className="cards">
         <Card />
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./styles.css";
+
+const Header: React.FC = () => {
+  return <h1>Header</h1>;
+};
+
+export default Header;
