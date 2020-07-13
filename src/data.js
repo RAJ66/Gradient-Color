bf2280438d323b33772c32abcb16fb26f3735bf2
Vitor <vitorbarbosa1998@hotmail.com>
2020-07-13 11:59:44 +0100

    doc: added gradient

--- src/data.js
+++ src/data.js
@@ -8,8 +8,16 @@ export const stockData = [
     color2: "#57370D",
   },
   {
-    color1: "#9600ff",
-    color2: "#aebaf8",
+    color1: "#ffe53b",
+    color2: "#00ffff",
+  },
+  {
+    color1: "#2193b0",
+    color2: "#6dd5ed",
+  },
+  {
+    color1: "#e21c34",
+    color2: "#500b28",
   },
   {
     color1: "#FF0A6C",
