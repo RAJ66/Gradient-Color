8842dc6cc0f293e47b9ab95fc1afe8b0bcc889db
Vitor <vitorbarbosa1998@hotmail.com>
2020-07-15 12:55:24 +0100

    style :typografi header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,7 +3,11 @@ import React from "react";
 import "./styles.css";
 
 const Header: React.FC = () => {
-  return <h1>Header</h1>;
+  return (
+    <div className="typewriter">
+      <h1> Header </h1>
+    </div>
+  );
 };
 
 export default Header;
