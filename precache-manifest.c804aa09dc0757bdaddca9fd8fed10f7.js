50b93587671d1755a7a40a9daa2ae923db5d34c6
Vitor <vitorbarbosa1998@hotmail.com>
2020-07-20 17:48:13 +0100

    Updates

---
+++ precache-manifest.c804aa09dc0757bdaddca9fd8fed10f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f51bc796edeeb889697bff0065517c2",
+    "url": "/Gradient-Color/index.html"
+  },
+  {
+    "revision": "549babbd355ecc61045f",
+    "url": "/Gradient-Color/static/css/main.f41f38b3.chunk.css"
+  },
+  {
+    "revision": "d8a91b0aa1df82389600",
+    "url": "/Gradient-Color/static/js/2.e7ecab21.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Gradient-Color/static/js/2.e7ecab21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "549babbd355ecc61045f",
+    "url": "/Gradient-Color/static/js/main.b1754071.chunk.js"
+  },
+  {
+    "revision": "e3b25c9a6bcaca144531",
+    "url": "/Gradient-Color/static/js/runtime-main.ead96fab.js"
+  }
+]);
